3d702a4a1aa5fcab236b80db5cd3145a6f05e43d
candasgenis <68709696+candasgenis@users.noreply.github.com>
2021-05-15 12:34:00 +0300

    PRICE_FILTER controls added, Code Optimized(unnecessary parts changed)

--- flaskMain.py
+++ flaskMain.py
@@ -4,8 +4,8 @@ from binance.client import Client
 from binance import BinanceSocketManager
 from binance.enums import *
 
-testnet_key = 'your_testnet_key'
-testnet_secret_key = 'your_testnet_secret_key'
+testnet_key = 'your_key'
+testnet_secret_key = 'your_secret_key'
 
 client = Client(testnet_key, testnet_secret_key, testnet=True)
 
@@ -14,43 +14,46 @@ app = Flask(__name__)
 @app.route('/openLimitOrder', methods=['POST'])
 def open_limit_order():
     try:
-        account_info = client.get_account()
+
 
         symbol = request.form.get("symbol")
         symbol_info = client.get_symbol_info(symbol)
 
-        isInWallet = False
+        for filter in symbol_info['filters']:
+            if filter['filterType'] == 'PERCENT_PRICE':
+                multiplierDown = float(filter['multiplierDown'])
+                multiplierUp = float(filter['multiplierUp'])
 
-        for asset in account_info['balances']:
-            symbol_in_wallet = asset['asset']
-            #print(symbol_in_wallet)
-            if symbol[:-4] == symbol_in_wallet:
-                print(symbol_in_wallet)
-                isInWallet = True
-                freeQty = float(asset['free'])
-        if isInWallet == False:
-            return jsonify({'status': 'You dont have this symbol in your wallet'})
-        quantity = request.form.get("quantity")
-        print(quantity)
-        price = request.form.get("price")
-        print(price)
-        multiplication = float(quantity) * float(price)
-        print(multiplication)
+        get_avg_price = client.get_avg_price(symbol=symbol)
+        avg_price = float(get_avg_price['price'])
+        print('avg_price: ', avg_price)
+
+        print('symbol: ' + symbol[:-4])
+        balance = client.get_asset_balance(asset=symbol[:-4])
+        freeQty = float(balance['free'])
+        print('freeQty: ', freeQty)
+        quantity = float(request.form.get("quantity"))
+        print('quantity: ', quantity)
+        price = float(request.form.get("price"))
+        print('price: ', price)
+        multiplication = quantity * price
+        print('multiplication: ', multiplication)
 
 
         minQty = float(symbol_info['filters'][2]['minQty'])
-        print(minQty)
+        print('minQty: ', minQty)
         maxQty = float(symbol_info['filters'][2]['maxQty'])
-        print(maxQty)
+        print('maxQty: ', maxQty)
 
         if request.form.get("side") == "buy":
             side = SIDE_BUY
         elif request.form.get("side") == "sell":
             side = SIDE_SELL
-        print(side)
+        print('side: ', side)
+
 
         if side == SIDE_BUY:
-            if (multiplication > 10) and (float(quantity) > minQty) and (float(quantity) < maxQty):
+            if (multiplication > 10) and (quantity > minQty) and (quantity < maxQty) and (avg_price*multiplierUp >= price) and (avg_price*multiplierDown <= price):
                 order = client.create_order(
                     symbol=symbol,
                     side=side,
@@ -61,10 +64,12 @@ def open_limit_order():
                 )
                 pprint.pprint(order)
                 return jsonify({'status': 'order placed'})
+            elif (avg_price*multiplierUp <= price) or (avg_price*multiplierDown >= price):
+                return jsonify({'status': 'PRICE FILTER ERROR'})
             else:
                 return jsonify({'status': 'doesnt meet requirements'})
         elif side == SIDE_SELL:
-            if ((float(quantity) < freeQty) and (float(quantity) > minQty) and (float(quantity) < maxQty)):
+            if (quantity < freeQty) and (quantity > minQty) and (quantity < maxQty) and (avg_price*multiplierUp >= price) and (avg_price*multiplierDown <= price):
                 order = client.create_order(
                     symbol=symbol,
                     side=side,
@@ -75,6 +80,8 @@ def open_limit_order():
                 )
                 pprint.pprint(order)
                 return jsonify({'status': 'order placed'})
+            elif (avg_price*multiplierUp <= price) or (avg_price*multiplierDown >= price):
+                return jsonify({'status': 'PRICE FILTER ERROR'})
             else:
                 return jsonify({'status': 'doesnt meet requirements'})
 
--- main.py
+++ main.py
@@ -2,25 +2,51 @@ import pprint
 from binance.client import Client
 from binance import BinanceSocketManager
 from binance.enums import *
+import time
 
-testnet_key = 'your_testnet_key'
-testnet_secret_key = 'your_testnet_secret_key'
+testnet_key = 'H0KabgOky36tbSXjTbSPc3kd9xR3f3WSqs5snBJI3wmy7gTOOxmumRDWKdysnZXA'
+testnet_secret_key = '8OKUmARWUoWkNMkIRo8lU8DlIPJkcxhqXPejvCOtDxcvT1xct8iat7MII138TauV'
+
+class Binance:
+    def __init__(self, public_key = testnet_key, secret_key = testnet_secret_key, sync = False):
+        self.time_offset = 0
+        self.b = Client(public_key, secret_key)
+        self.b.API_URL = 'https://testnet.binance.vision/api' # for testnet
+
+        if sync:
+            self.time_offset = self._get_time_offset()
+            print( "Offset: %s ms" % (self.time_offset) )
+
+    def _get_time_offset(self):
+        res = self.b.get_server_time()
+        return res['serverTime'] - int(time.time() * 1000)
+
+    def synced(self, fn_name, **args):
+        args['timestamp'] = int(time.time() * 1000 + self.time_offset)
+        return getattr(self.b, fn_name)(**args)
+
 
 client = Client(testnet_key, testnet_secret_key, testnet=True)
+my_binance = Binance(testnet_key, testnet_secret_key, True)
+# my_binance.synced('order_market_buy',  symbol='BNBBTC', quantity=10)
+account_info = my_binance.synced('get_account', recvWindow=60000)
+
 
 #Market Data Endpoints
-depth = client.get_order_book(symbol='XRPBUSD')
-res = client.get_exchange_info()
-time_res = client.get_server_time()
-status = client.get_system_status()
-symbol_info = client.get_symbol_info('BTCBUSD')
-allcoins_info = client.get_all_tickers()
-avg_price = client.get_avg_price(symbol='XRPBUSD')
-prices = client.get_all_tickers()
-orderbook_tickers = client.get_orderbook_tickers()
+#depth = client.get_order_book(symbol='XRPBUSD')
+#res = client.get_exchange_info()
+#time_res = client.get_server_time()
+#status = client.get_system_status()
+symbol_info = client.get_symbol_info('XRPBTC')
+#allcoins_info = client.get_all_tickers()
+avg_price = client.get_avg_price(symbol='XRPBTC')
+#prices = client.get_all_tickers()
+#orderbook_tickers = client.get_orderbook_tickers()
+#candles = client.get_klines(symbol='BTCBUSD', interval=Client.KLINE_INTERVAL_5MINUTE)
 
 #Account Endpoints
-account_info = client.get_account()
+#account_info = client.get_account()
+#balance = client.get_asset_balance(asset='BTC')
 #open order
 """order = client.create_order(
     symbol='BTCBUSD',
@@ -30,17 +56,23 @@ account_info = client.get_account()
     quantity=0.001,
     price=10000
     )"""
-orders = client.get_all_orders(symbol='BTCBUSD')
+#orders = client.get_all_orders(symbol='BTCBUSD')
 #delete order
 """result = client.cancel_order(
     symbol='BTCBUSD',
     orderId='1787')"""
-open_orders = client.get_open_orders(symbol='BTCBUSD')
+#open_orders = client.get_open_orders(symbol='BTCBUSD')
 
 #account_snapshot = client.get_account_snapshot(type='SPOT')
 
+for filter in symbol_info['filters']:
+    if filter['filterType'] == 'PERCENT_PRICE':
+        multiplierDown = float(filter['multiplierDown'])
+        pprint.pprint(multiplierDown)
+        multiplierUp = float(filter['multiplierUp'])
+        pprint.pprint(multiplierUp)
 
-pprint.pprint(account_info)
+pprint.pprint(avg_price)
 
 """for asset in account_info['balances']:
     symbol_in_wallet = asset['asset']
